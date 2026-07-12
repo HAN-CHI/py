70fa8b3ab9c07ec54206e838fa74346d80217a13
HAN-CHI <34908619+HAN-CHI@users.noreply.github.com>
2026-07-13 05:54:00 +0800

    Update fengshui_lib.py

--- fengshui_lib.py
+++ fengshui_lib.py
@@ -1,3 +1,6 @@
+from fengshui_db.py import PENGZU_STEMS, PENGZU_BRANCHES
+
+
 class CalendarEngine:
     @staticmethod
     def get_jianchu(lunar_date_idx):
@@ -9,3 +12,11 @@ class CalendarEngine:
     def get_constellation(date_delta):
         """根據與基點日期的天數差，推算二十八宿"""
         return CALENDAR_RULES["Constellations"]["sequence"][date_delta % 28]
+
+class PengZuEngine:
+    @staticmethod
+    def get_taboo(gan, zhi):
+        """輸入日干與日支，返回彭祖百忌說明"""
+        stem_taboo = PENGZU_STEMS.get(gan, "")
+        branch_taboo = PENGZU_BRANCHES.get(zhi, "")
+        return [stem_taboo, branch_taboo]
